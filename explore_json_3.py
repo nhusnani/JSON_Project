27071bdd3aa15fac3c90d582abf5b86ec3fb3a22
Noureen Husnani <Noureen_Husnani1@baylor.edu>
2021-03-01 15:36:16 -0600

    Commit

---
+++ explore_json_3.py
@@ -0,0 +1,57 @@
+import json
+
+infile = open('eq_data_1_day_m1.json', 'r')
+outfile = open('readable_eq_data.json','w')
+
+# The json.load() function converts the data into a format Python
+# can work with: in this case, a giant dictionary.
+eq_data = json.load(infile)
+
+json.dump(eq_data, outfile, indent = 4)
+
+list_of_eqs = eq_data['features']
+
+mags, lons, lats, hover_texts = [], [], [], []
+
+for eq in list_of_eqs:
+    mag = eq['properties']['mag']
+    lon = eq['geometry']['coordinates'][0]
+    lat = eq['geometry']['coordinates'][1]
+    hover_text = eq['properties']['place']
+
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+    hover_texts.append(hover_text)
+
+print(mags[:10])
+print(lons[:10])
+print(lats[:10])
+print(hover_texts[:10])
+
+
+from plotly.graph_objs import Scattergeo, Layout # Scattergeo allows to use scatter points and plot on a map
+from plotly import offline
+
+data = [{
+    'type': 'scattergeo',
+    'lon':lons,
+    'lat':lats,
+    'text': hover_texts,
+    'marker':{
+        'size': [5 * mag for mag in mags],
+        'color': mags,
+        'colorscale':'viridis',
+        'reversescale': True,
+        'colorbar': {"title":"Magnitude"}
+    }
+}]
+
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+
+offline.plot(fig, filename='global_earthquakes.html')
+
+
+#print(eq_data['features'][0]['properties']['mag'])
